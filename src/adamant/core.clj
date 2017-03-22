4e761addcaebeba4af79073f3494742ad0da30c0
mynomoto <mynomoto@gmail.com>
2017-03-22 01:26:14 -0300

    Sample parsed namespace requires

--- src/adamant/core.clj
+++ src/adamant/core.clj
@@ -1,7 +1,9 @@
 (ns adamant.core
   (:require
-    [clojure.string :as str]
-    [clojure
+    clojure.string
+    [clojure.java
+     [io :as io :refer [reader]]]
+    [clojure set walk
      [set :as set :refer [union]]
      [walk :as walk]]
     [lucid.query :as q]
@@ -12,4 +14,48 @@
 
 (def fragment {:file "src/adamant/core.clj"})
 
-(s/conform ::ns (rest (first (q/$ fragment [ns]))))
+(def conformed
+  (s/conform ::ns (rest (first (q/$ fragment [ns])))))
+
+'{:name adamant.core
+ :clauses [[:require {:clause :require
+                      :libs [[:prefix-list {:prefix clojure.string
+                                            :refer {:as str}}]
+                             [:prefix-list {:prefix clojure.java
+                                            :suffix [[:prefix-list {:prefix io
+                                                                    :refer {:as io
+                                                                            :refer [:syms [reader]]}}]]}]
+                             [:prefix-list {:prefix clojure
+                                            :suffix [[:prefix-list {:prefix set
+                                                                    :refer {:as set
+                                                                            :refer [:syms [union]]}}]
+                                                     [:prefix-list {:prefix walk
+                                                                    :refer {:as walk}}]]}]
+                             [:prefix-list {:prefix lucid.query
+                                            :refer {:as q}}]
+                             [:prefix-list {:prefix clojure.core.specs
+                                            :refer {:as core-specs}}]
+                             [:prefix-list {:prefix clojure.spec
+                                            :refer {:as s}}]]}]]}
+
+'{:name adamant.core
+ :clauses [[:require {:clause :require
+                      :libs [[:lib clojure.string]
+                             [:prefix-list {:prefix clojure.java
+                                            :suffix [[:prefix-list {:prefix io
+                                                                    :refer {:as io
+                                                                            :refer [:syms [reader]]}}]]}]
+                             [:prefix-list {:prefix clojure
+                                            :suffix [[:lib set]
+                                                     [:lib walk]
+                                                     [:prefix-list {:prefix set
+                                                                    :refer {:as set
+                                                                            :refer [:syms [union]]}}]
+                                                     [:prefix-list {:prefix walk
+                                                                    :refer {:as walk}}]]}]
+                             [:prefix-list {:prefix lucid.query
+                                            :refer {:as q}}]
+                             [:prefix-list {:prefix clojure.core.specs
+                                            :refer {:as core-specs}}]
+                             [:prefix-list {:prefix clojure.spec
+                                            :refer {:as s}}]]}]]}
